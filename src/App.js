3b040c2254c9527e9a5c85cbd5a302a01b0be62e
IrinaGM <irina.golionko@gmail.com>
2019-12-19 21:34:13 -0500

    created sign-in form, custom-button and custom form input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
+
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -12,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
